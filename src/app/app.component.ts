29f7992f6e34bf7f581473de973512ca2c00eaba
Gabriel Geraldino <gabriel_geraldinos@outlook.com>
2023-01-21 19:53:18 -0300

    First

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   constructor() {}
+  url = 'http://localhost:4200/mfe.js';
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,19 +1,21 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
-
+import { LazyElementsModule } from '@angular-extensions/elements';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    LazyElementsModule,
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class HomePageModule {}
